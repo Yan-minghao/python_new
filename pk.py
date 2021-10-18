24d2b2fb877e823d0e5d32b1c622d580e05ebca7
yanminghao <ymh254089@163.com>
2021-10-18 17:29:01 +0800

    add pk project

---
+++ pk.py
@@ -0,0 +1,49 @@
+# 1.身份选择
+
+
+aggressivity=2# 攻击力
+life=2# 生命值
+print('请选择你的身份：')
+print('\t1、唐僧')
+print('\t2、白骨精')
+num= int(input('请选择(1-2)'))
+if num ==1 :
+    print('你已选择唐僧,恭喜你将以唐僧身份进行游戏')
+elif num==2 :
+    print('')
+else: print('')
+
+isCard =True
+while isCard :
+    print('请选择你要的操作：')
+    print('         1、练级：')
+    print('         2、打Boss')
+    print('         3、逃跑')
+    select=int(input('请选择(1-3):'))
+
+    if select==1 :
+        aggressivity+=2
+        life+=2
+        print(f"你的身份是唐僧，你的攻击力是:{aggressivity} 你的生命值是: {life}")
+    elif select==2 :
+        print('开始大boss')
+
+        print(f"你的身份是唐僧，你的攻击力是:{aggressivity} 你的生命值是: {life}")
+    elif select==3:
+        print('逃跑了')
+        isCard=False
+        break
+
+
+
+
+
+#
+# isCard =True
+# while isCard :
+#     print('请选择你的身份：')
+#     print('1、唐僧')
+#     print('2、白骨精')
+#     num= int(input('请选择(1-2)'))
+#
+#
