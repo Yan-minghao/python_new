34239274d09a7dc7c0ba10f30ff8326f3c6d3ae9
yanminghao <ymh254089@163.com>
2021-10-19 13:41:21 +0800

    add tuple 元组study

---
+++ tuple.py
@@ -0,0 +1,21 @@
+# 元组
+my_tuple=(10,20,30,40)
+
+# print(my_tuple[3])
+#
+# my_tuple=40,
+# print(type(my_tuple))
+#交换 a b 变量
+# my_tuple=(10,20)
+# a,b=my_tuple
+# print('a',a)
+# print('b',b)
+# print('-'*30)
+# a,b=b,a
+# print('交换后')
+# print('a',a)
+# print('b',b)
+
+# a,b,c,d=my_tuple
+
+a,b,*c=my_tuple
