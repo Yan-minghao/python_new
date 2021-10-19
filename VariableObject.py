bd66382774c22486fc33efbb6cc851fe22f0c36b
yanminghao <ymh254089@163.com>
2021-10-19 14:11:17 +0800

    is is not == != 区别

--- VariableObject.py
+++ VariableObject.py
@@ -1,8 +1,15 @@
 # 可变对象
-print()
-obj = [1, 2, 3]
-print('obj修改前： ', obj,id(obj))
-# obj[0]=100
-obj=[000,1]
-print('obj修改后： ', obj,id(obj))
+# print()
+# obj = [1, 2, 3]
+# print('obj修改前： ', obj,id(obj))
+# # obj[0]=100
+# obj=[000,1]
+# print('obj修改后： ', obj,id(obj))
 
+# == != 比较值是否相等
+# is is not 比较对象是否相等
+
+a=[1]
+b=[1]
+print(a==b)
+print(a is b)
