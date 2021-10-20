d034f9ea92e9faef4b167244df88337feb38ce2e
yanminghao <ymh254089@163.com>
2021-10-20 14:01:27 +0800

    commit  函数返回值

---
+++ return.py
@@ -0,0 +1,15 @@
+# 返回值 return fn1 表示对象fn1  fn1() 表示函数
+def fn1() :
+    return
+# print(fn1)
+# print(fn1())
+
+def sum(*d):
+    a=0
+    for i in d:
+        a+=i
+    return a
+
+
+print(sum(1, 3, 10, -1000))
+
